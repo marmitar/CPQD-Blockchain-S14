f7b0153b371ca36b7fcfa73aed6da72469a55470
Tiago de Paula <tiagodepalves@gmail.com>
2025-06-25 19:12:00 -0300

    fix: shellcheck issues

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")"
 # Verifica se as variaveis de ambiente foram setadas
 if [ -z "${SGX_SDK}" ]; then
     # Carrega as ferramentas e variaveis de ambiente do SGX-SDK
+    # shellcheck source=/dev/null
     source /opt/intel/sgxsdk/environment
 fi
 
@@ -49,8 +50,8 @@ declare -a ENCLAVE_FLAGS=(
     -nostdinc++
 )
 declare -a ENCLAVE_LINK_FLAGS=(
-    -Wl,-z,relro,-z,now,-z,noexecstack
-    -Wl,--no-undefined
+    '-Wl,-z,relro,-z,now,-z,noexecstack'
+    '-Wl,--no-undefined'
     -nostdlib -nodefaultlibs -nostartfiles "-L${SGX_SDK}/lib64"
 	'-Wl,--whole-archive'
     -lsgx_trts_sim
@@ -85,17 +86,17 @@ declare -a GENERATED_FILES=(
 ################
 
 # PASSO 1: Remover arquivos gerados automaticamente.
-rm -f ${GENERATED_FILES[@]}
- 
+rm -f "${GENERATED_FILES[@]}"
+
 # PASSO 2: Gerar interfaces entre Untrusted Components e Enclaves.
 pushd App
 sgx_edger8r --untrusted ../Enclave/Enclave.edl --search-path ../Enclave --search-path "${SGX_SDK}/include"
 popd
 
 # PASSO 3: Compilar APP.
-$CC ${SGX_COMMON_CFLAGS[@]} ${APP_FLAGS[@]} -c App/Enclave_u.c -o App/Enclave_u.o
-g++ ${SGX_COMMON_CXXFLAGS[@]} ${APP_FLAGS[@]} -c App/App.cpp -o App/App.o
-g++ ${SGX_COMMON_CXXFLAGS[@]} ${APP_FLAGS[@]} -c App/TrustedLibrary/Libcxx.cpp -o App/TrustedLibrary/Libcxx.o
+$CC "${SGX_COMMON_CFLAGS[@]}" "${APP_FLAGS[@]}" -c App/Enclave_u.c -o App/Enclave_u.o
+g++ "${SGX_COMMON_CXXFLAGS[@]}" "${APP_FLAGS[@]}" -c App/App.cpp -o App/App.o
+g++ "${SGX_COMMON_CXXFLAGS[@]}" "${APP_FLAGS[@]}" -c App/TrustedLibrary/Libcxx.cpp -o App/TrustedLibrary/Libcxx.o
 g++ App/Enclave_u.o App/App.o App/TrustedLibrary/Libcxx.o -o app -L/opt/intel/sgxsdk/lib64 -lsgx_urts_sim -lpthread
 
 ####################
@@ -107,10 +108,10 @@ pushd Enclave
 popd
 
 # PASSO 5: Compilar ENCLAVE
-$CC -m64 -O2 -Wall -Wextra -Winit-self -Wpointer-arith -Wreturn-type -Waddress -Wsequence-point -Wformat-security -Wmissing-include-dirs -Wfloat-equal -Wundef -Wshadow -Wcast-align -Wcast-qual -Wconversion -Wredundant-decls -Wjump-misses-init -Wstrict-prototypes -Wunsuffixed-float-constants -nostdinc -fvisibility=hidden -fpie -fstack-protector -fno-builtin-printf ${ENCLAVE_INCLUDES[@]}  -c Enclave/Enclave_t.c -o Enclave/Enclave_t.o
-g++ ${SGX_COMMON_CXXFLAGS[@]} ${ENCLAVE_FLAGS[@]} -c Enclave/Enclave.cpp -o Enclave/Enclave.o
-g++ ${SGX_COMMON_CXXFLAGS[@]} ${ENCLAVE_FLAGS[@]} -c Enclave/TrustedLibrary/Libcxx.cpp -o Enclave/TrustedLibrary/Libcxx.o
-g++ Enclave/Enclave_t.o Enclave/Enclave.o Enclave/TrustedLibrary/Libcxx.o -o enclave.so ${ENCLAVE_LINK_FLAGS[@]}
+$CC -m64 -O2 -Wall -Wextra -Winit-self -Wpointer-arith -Wreturn-type -Waddress -Wsequence-point -Wformat-security -Wmissing-include-dirs -Wfloat-equal -Wundef -Wshadow -Wcast-align -Wcast-qual -Wconversion -Wredundant-decls -Wjump-misses-init -Wstrict-prototypes -Wunsuffixed-float-constants -nostdinc -fvisibility=hidden -fpie -fstack-protector -fno-builtin-printf "${ENCLAVE_INCLUDES[@]}"  -c Enclave/Enclave_t.c -o Enclave/Enclave_t.o
+g++ "${SGX_COMMON_CXXFLAGS[@]}" "${ENCLAVE_FLAGS[@]}" -c Enclave/Enclave.cpp -o Enclave/Enclave.o
+g++ "${SGX_COMMON_CXXFLAGS[@]}" "${ENCLAVE_FLAGS[@]}" -c Enclave/TrustedLibrary/Libcxx.cpp -o Enclave/TrustedLibrary/Libcxx.o
+g++ Enclave/Enclave_t.o Enclave/Enclave.o Enclave/TrustedLibrary/Libcxx.o -o enclave.so "${ENCLAVE_LINK_FLAGS[@]}"
 
 # PASSO 6: Assinar ENCLAVE
 /opt/intel/sgxsdk/bin/x64/sgx_sign sign \
